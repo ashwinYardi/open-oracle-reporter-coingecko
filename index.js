5f01013b467bf46c37812f5f25ef1e89920241de
ashwinYardi <ashwin.yar@live.com>
2020-11-01 18:13:43 +0530

    add config and dotenv to fetch and sign dynamically

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
+require("dotenv").config();
+const config = require("config");
 const { ExpressEndpoint } = require("open-oracle-reporter");
 const CoinGecko = require("coingecko-api");
+
 const CoinGeckoClient = new CoinGecko();
+const coins = config.get("coins");
+const vsCurrency = config.get("vsCurrency");
+
+async function fetchPrices(now) {
+  const coingeckoIds = coins.map((coin) => {
+    return coin.id;
+  });
+
+  const prices = (
+    await CoinGeckoClient.simple.price({
+      ids: coingeckoIds.join(),
+      vs_currencies: vsCurrency,
+    })
+  ).data;
+
+  const priceFeed = {};
+
+  coins.forEach((coin) => {
+    priceFeed[coin.symbol] = prices[coin.id][vsCurrency];
+  });
+
+  return [now, priceFeed];
+}
 
 const app = ExpressEndpoint.endpoint(process.env.PRIVATE_KEY, fetchPrices);
 
-const assets = ["ethereum", "compound-governance-token", "dai", "unifi"];
-
-async function fetchPrices(now) {
-  const prices = await CoinGeckoClient.simple.price({
-    ids: assets.join(),
-    vs_currencies: "usd",
-  });
-  console.log(prices);
-  return [
-    now,
-    {
-      eth: prices.data.ethereum.usd,
-      comp: prices.data[`compound-governance-token`].usd,
-      dai: prices.data.dai.usd,
-    },
-  ];
-}
-
-app.listen(8000, () => {
-  console.log(`Example app listening at 8000`);
+app.listen(process.env.PORT, () => {
+  console.log(`Reporter started. Server is listening at ${process.env.PORT}`);
 });
